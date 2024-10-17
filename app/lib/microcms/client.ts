868e30ef028f88026fe022d48115614d1af89b2a
yukinagao719 <ynagao719@gmail.com>
2024-10-17 11:10:23 +0900

    done

--- app/lib/microcms/client.ts
+++ app/lib/microcms/client.ts
@@ -9,6 +9,9 @@ export const client = createClient({
 export const getAllBooks = async () => {
   const allBooks = await client.getList<BookType>({
     endpoint: "bookcommerce",
+    customRequestInit: {
+      cache: "no-store"
+    }
   });
   return allBooks;
 };
@@ -17,6 +20,9 @@ export const getDetailBook = async (contentId: string) => {
 const detailBook = await client.getListDetail<BookType>({
   endpoint: "bookcommerce",
   contentId,
+  customRequestInit: {
+    cache: "no-store"
+  }
 })
 return detailBook
 }
